97d42a39affa585dcdd80bf824c00ec197b52f7e
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-16 17:56:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,7 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
     if position_1 = "X" && position_2 = "X" && position_3 = "X"
-      win_combination
+      return win_combination
     end
   end
 end
